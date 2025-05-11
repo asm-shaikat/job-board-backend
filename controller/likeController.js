4332571cdcb44e00516c27540d9b972120d46876
asm-shaikat <asmshaikat1952@gmail.com>
2025-05-11 13:32:13 +0600

    toggle like bug fix

--- controller/likeController.js
+++ controller/likeController.js
@@ -2,23 +2,27 @@ const Like = require("../models/Like");
 const Job = require("../models/Job");
 
 exports.toggleLike = async (req, res) => {
+  try {
     const jobId = req.params.jobId;
-    const userId = req.user.id; // this depends on your auth middleware
-  
-    try {
-      const existingLike = await Like.findOne({ job: jobId, user: userId });
-  
-      if (existingLike) {
-        await existingLike.remove();
-        return res.status(200).json({ message: "Unliked job" });
-      }
-  
-      const newLike = await Like.create({ job: jobId, user: userId });
-      res.status(201).json({ message: "Liked job", like: newLike });
-    } catch (err) {
-      res.status(500).json({ message: err.message });
+    const userId = req.user.id;
+
+    console.log("JobID:", jobId);
+    console.log("UserID:", userId);
+
+    const existingLike = await Like.findOne({ job: jobId, user: userId });
+
+    if (existingLike) {
+      await Like.deleteOne({ _id: existingLike._id });
+      return res.status(200).json({ message: "Unliked job" });
     }
-  };
+
+    const newLike = await Like.create({ job: jobId, user: userId });
+    res.status(201).json({ message: "Liked job", like: newLike });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
 
 exports.getLikesByJob = async (req, res) => {
   try {
